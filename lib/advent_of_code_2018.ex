e190b118baf9a011bc124951145875a0d98b4c73
Adam Mokan <amokan@gmail.com>
2018-12-18 10:35:54 -0700

    day two completed

--- lib/advent_of_code_2018.ex
+++ lib/advent_of_code_2018.ex
@@ -7,9 +7,11 @@ defmodule AdventOfCode2018 do
   Run the solutions for all days
   """
   def run_all do
-    IO.puts "--- Advent of Code 2018 ---"
+    IO.puts "--- Advent of Code 2018 ---\n"
 
     day1() |> IO.inspect(label: "Day 1 Result")
+
+    day2() |> IO.inspect(label: "Day 2 Result")
 
     IO.puts "\n\n"
   end
@@ -19,5 +21,11 @@ defmodule AdventOfCode2018 do
   Runs the day 1 solution
   """
   def day1, do: AdventOfCode2018.Day1.Solution.run()
+
+
+  @doc """
+  Runs the day 2 solution
+  """
+  def day2, do: AdventOfCode2018.Day2.Solution.run()
 
 end
---
+++ lib/advent_of_code_2018/day2/solution.ex
@@ -0,0 +1,39 @@
+defmodule AdventOfCode2018.Day2.Solution do
+  @moduledoc """
+  Solution for Day 2 AoC problem
+  """
+
+  @input_file "priv/day2_input.txt"
+
+  @doc """
+  Return the solution for the input file.
+  """
+  def run do
+    File.stream!(@input_file)
+    |> Stream.map(&(String.replace(&1, "\n", "")))
+    |> Stream.map(&(String.split(&1, "", trim: true)))
+    # count occurrences of each letter
+    |> Stream.map(fn l ->
+      Enum.reduce(l, %{}, fn letter, acc ->
+        Map.update(acc, letter, 1, &(&1 + 1))
+      end)
+    end)
+    # filter out all items that only have one letter occurance
+    |> Stream.map(fn m -> :maps.filter(fn _, v -> v > 1 end, m) end)
+    # grab just the values back from the counted maps per input row
+    |> Stream.map(&(Map.values(&1)))
+    # uniq each count per input row
+    |> Stream.map(&(Enum.uniq(&1)))
+    # turn the stream into a list of counts
+    |> Enum.to_list()
+    # flatten the list of lists (counts)
+    |> List.flatten()
+    # count the occurrances of each count
+    |> Enum.reduce(%{}, fn(n, acc) -> Map.update(acc, n, 1, &(&1 + 1)) end)
+    # extract the values from the map of counts
+    |> Map.values()
+    # multiply the results
+    |> Enum.reduce(&*/2)
+  end
+
+end
